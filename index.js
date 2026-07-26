035090aea78923831ab4dfe2a59ad20f2d99b20e
stdBude <itstd.6146@uob.edu.ly>
2026-07-26 10:24:26 +0200

    added the production url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dns.setServers(["8.8.8.8", "1.1.1.1"])
 const app = express();
 app.use(cors({
     origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:5173', 'https://municipality-frontend-prototype.onrender.com', "https://municipality-frontend-prototype-kjfgbalr8-bude-team.vercel.app"];
+    const allowedOrigins = ['http://localhost:5173', 'https://municipality-frontend-prototype.onrender.com', "https://municipality-frontend-prototype-kjfgbalr8-bude-team.vercel.app","https://municipality-frontend-prototype.vercel.app"];
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
         callback(null, true);
     } else {
